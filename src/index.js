73744a558c11472c9ee95dae624404570e60cdaf
Stinker707 <164636323+Stinker707@users.noreply.github.com>
2024-07-05 23:04:45 +0300

    настроил webpack. переписал css на SCSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-const btn = document.querySelector('#close-btn');
-const menuMobi = document.querySelector('#menu-mobi');
-btn.onclick = () => {
-    btn.classList.toggle('header-mobi-close-btn--is-active');
-    menuMobi.classList.toggle('header-mobi__menu--is-visible');
-}
+import './index.html';
+import './index.scss';
+import { sum } from './modules/calc';
+
+console.log(sum(3, 4));
